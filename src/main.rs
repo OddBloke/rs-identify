9eddfe007a3b281d938eadff932b5ab50d785e58
Daniel Watkins <daniel@daniel-watkins.co.uk>
2020-05-16 19:22:57 -0400

    initial implementation
    This is very rough, of course, but it passes two of cloud-init's
    ds-identify tests (the AliYun ones), so it has a framework that
    works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,140 @@
+use std::collections::BTreeMap;
+use std::fs::{create_dir_all, File};
+use std::io::Write;
+use std::path::PathBuf;
+
+struct RsIdentify {
+    // Paths
+    path_root: PathBuf,
+    cfg_out: PathBuf,
+}
+
+impl RsIdentify {
+    // Setup
+    fn new(path_root: PathBuf) -> RsIdentify {
+        let mut cfg_out = PathBuf::from(path_root.clone());
+        cfg_out.push("run/cloud-init/cloud.cfg");
+
+        // Emit our paths/settings
+        println!("PATH_ROOT: {}", path_root.display());
+        println!("CFG_OUT: {}", cfg_out.display());
+
+        RsIdentify { path_root, cfg_out }
+    }
+
+    fn from_env() -> RsIdentify {
+        let path_root = match std::env::var("PATH_ROOT") {
+            Ok(val) => PathBuf::from(&val),
+            Err(_) => std::process::exit(1),
+        };
+        RsIdentify::new(path_root)
+    }
+
+    // Datasource checks
+    fn dscheck_AliYun(&self) -> bool {
+        // TODO: seed directory checks
+        dmi_product_name() == "Alibaba Cloud ECS"
+    }
+
+    // Output
+    fn write_cfg_out(self, datasource_list: Vec<String>) {
+        create_dir_all(self.cfg_out.parent().unwrap()).unwrap();
+        let mut file = match File::create(&self.cfg_out) {
+            Err(why) => panic!(
+                "couldn't create {}: {}",
+                self.cfg_out.display(),
+                why.to_string()
+            ),
+            Ok(file) => file,
+        };
+        let mut map = BTreeMap::new();
+        map.insert("datasource_list".to_string(), datasource_list);
+        if file
+            .write_all(serde_yaml::to_string(&map).unwrap().as_bytes())
+            .is_err()
+        {
+            std::process::exit(1);
+        };
+    }
+
+    fn get_datasource_list_from_path(&self, path: &PathBuf) -> Option<Vec<String>> {
+        let file = match File::open(&path) {
+            Err(_) => return None,
+            Ok(file) => file,
+        };
+        let config: serde_yaml::Mapping = serde_yaml::from_reader(file).unwrap();
+        config
+            .get(&serde_yaml::Value::from("datasource_list"))
+            .map(|datasource_list| {
+                datasource_list
+                    .as_sequence()
+                    .unwrap()
+                    .iter()
+                    .filter_map(|value| value.as_str().map(|s| s.to_string()))
+                    .collect()
+            })
+    }
+
+    fn get_datasource_list(&self) -> Vec<String> {
+        // Set up all our paths first
+        let mut etc_cloud_path = PathBuf::from(self.path_root.clone());
+        etc_cloud_path.push("etc/cloud/cloud.cfg");
+        let mut etc_cloud_d_path = PathBuf::from(self.path_root.clone());
+        etc_cloud_d_path.push("etc/cloud/cloud.cfg.d");
+        let mut cloud_d_paths: Vec<PathBuf> = match std::fs::read_dir(etc_cloud_d_path) {
+            Err(_) => vec![],
+            Ok(read_dir) => read_dir
+                .filter_map(|dir_entry| dir_entry.ok().map(|dir_entry| dir_entry.path()))
+                .collect(),
+        };
+        cloud_d_paths.sort();
+
+        // Find the latest definition of datasource_list and use that
+        // TEST GAP: the tests don't exercise checking cloud.cfg itself
+        let mut list = self.get_datasource_list_from_path(&etc_cloud_path);
+        for cloud_d_path in cloud_d_paths {
+            list = self.get_datasource_list_from_path(&cloud_d_path).or(list);
+        }
+        list.unwrap_or(vec!["AliYun".to_string()])
+    }
+
+    // Identify
+    fn identify(self) {
+        // Identify!
+        let input_datasource_list = self.get_datasource_list();
+
+        let mut output_datasource_list = vec![];
+        for candidate_datasource in input_datasource_list {
+            match candidate_datasource.as_str() {
+                "AliYun" => {
+                    if self.dscheck_AliYun() {
+                        output_datasource_list.push("AliYun".to_string());
+                    }
+                }
+                _ => (),
+            };
+            println!("{}", candidate_datasource);
+        }
+
+        if !output_datasource_list.contains(&"None".to_string()) {
+            output_datasource_list.push("None".to_string());
+        };
+
+        // Persist
+        self.write_cfg_out(output_datasource_list);
+    }
+}
+
+// Helpers
+
+fn dmi_product_name() -> String {
+    "Alibaba Cloud ECS".to_string()
+}
+
+// Datasource checks
+
 fn main() {
-    println!("Hello, world!");
+    // Determine our paths/settings
+    let rs_identify = RsIdentify::from_env();
+    rs_identify.identify()
 }
